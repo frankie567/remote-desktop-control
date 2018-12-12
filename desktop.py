f31fffe40e8d7e3253a21cd790325c3089158491
François Voron <fvoron@gmail.com>
2018-12-12 15:48:24 +0100

    First implementation

---
+++ desktop.py
@@ -0,0 +1,58 @@
+import asyncio
+import json
+import logging
+import psutil
+import sys
+import websockets
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def json_to_payload(message):
+    return json.dumps(message)
+
+async def cpu_usage_reporter(websocket):
+    psutil.cpu_percent()
+    while (True):
+        await asyncio.sleep(1)
+        message = {
+            'event': 'cpu',
+            'value': psutil.cpu_percent(),
+        }
+        await websocket.send(json_to_payload(message))
+        logger.debug(f'Sent message to server: {message}')
+
+async def consumer(message):
+    json_message = json.loads(message)
+    logger.debug(f'Server message received: {json_message}')
+
+    if (json_message['event'] == 'beep'):
+        print("\a")
+
+async def consumer_handler(websocket):
+    async for message in websocket:
+        await consumer(message)
+
+async def handler(uri, client_id):
+    async with websockets.connect(uri) as websocket:
+        message = {
+            'event': 'authentication',
+            'client_id': client_id,
+            'client_mode': 'desktop'
+        }
+        await websocket.send(json_to_payload(message))
+
+        consumer_task = asyncio.ensure_future(
+        consumer_handler(websocket))
+        producer_task = asyncio.ensure_future(
+            cpu_usage_reporter(websocket))
+        done, pending = await asyncio.wait(
+            [consumer_task, producer_task],
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for task in pending:
+            task.cancel()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(handler('ws://localhost:8000/ws', sys.argv[1]))
---
+++ server.py
@@ -0,0 +1,57 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.websockets import WebSocketDisconnect
+import json
+import logging
+import uvicorn
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+app = Starlette()
+
+websockets = {
+    'web': {},
+    'desktop': {},
+}
+
+async def receive_json(websocket):
+    message = await websocket.receive_text()
+    return json.loads(message)
+
+@app.websocket_route('/ws')
+async def websocket_endpoint(websocket):
+    await websocket.accept()
+
+    # "Authentication" message
+    message = await receive_json(websocket)
+    client_mode = message['client_mode']
+    client_id = message['client_id']
+    websockets[client_mode][client_id] = websocket
+    
+    # Get mirror mode to broadcast messages to the client on the other side
+    mirror_mode = 'web' if client_mode == 'desktop' else 'desktop'
+
+    client_string = f'{client_id}[{client_mode}]'
+    logger.info(f'Client connected: {client_string}')
+
+    while (True):
+        try:
+            # Wait for a message from the client
+            message = await receive_json(websocket)
+            logger.debug(f'Message received from {client_string}: {message}')
+
+            try:
+                # Broadcast it to the mirror client
+                await websockets[mirror_mode][client_id].send_text(json.dumps(message))
+            except KeyError:
+                logger.debug(f'Client {client_id}[{mirror_mode}] not connected')
+        except WebSocketDisconnect:
+            break
+
+    del websockets[client_mode][client_id]
+    await websocket.close()
+    logger.info(f'Client disconnected: {client_string}')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
